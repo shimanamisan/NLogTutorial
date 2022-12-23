bc4c6cc4d625141d0e86df3ed34474411817d6ae
shimanamisan <mikan.sup.all@gmail.com>
2022-12-23 18:26:28 +0900

    update: インデントを4に修正

--- NLogTutorial/MainWindow.xaml.cs
+++ NLogTutorial/MainWindow.xaml.cs
@@ -5,62 +5,62 @@ using System.Windows;
 namespace NLogTutorial
 {
 
-  public partial class MainWindow : Window
-  {
-    static Logger _logger = LogManager.GetCurrentClassLogger();
-
-    private bool _isLogStop = false;
-
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
+        static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        private bool _isLogStop = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Log_Output(object sender, RoutedEventArgs e)
+        {
+            _isLogStop = true;
+
+            StartLoopInfoLog();
+        }
+
+        private void Log_Exception(object sender, RoutedEventArgs e)
+        {
+            _isLogStop = true;
+
+            StartLoopErrorLog();
+        }
+
+        private void Log_Rotation(object sender, RoutedEventArgs e)
+        {
+            _isLogStop = false;
+
+            MessageBox.Show("ループ処理を停止しました。");
+        }
+
+        private async void StartLoopInfoLog()
+        {
+            while (_isLogStop)
+            {
+                _logger.Info("Infoを一回出力します");
+                _logger.Warn("Warnを一回出力します");
+                _logger.Error("Errorを一回出力します");
+                _logger.Fatal("Fatalを一回出力します");
+
+                await Task.Delay(0);
+            }
+        }
+
+        private async void StartLoopErrorLog()
+        {
+            while (_isLogStop)
+            {
+                _logger.Info("Infoを一回出力します");
+                _logger.Warn("Warnを一回出力します");
+                _logger.Error("Errorを一回出力します");
+                _logger.Fatal("Fatalを一回出力します");
+
+                await Task.Delay(0);
+            }
+        }
     }
-
-    private void Log_Output(object sender, RoutedEventArgs e)
-    {
-      _isLogStop = true;
-
-      StartLoopInfoLog();
-    }
-
-    private void Log_Exception(object sender, RoutedEventArgs e)
-    {
-      _isLogStop = true;
-
-      StartLoopErrorLog();
-    }
-
-    private void Log_Rotation(object sender, RoutedEventArgs e)
-    {
-      _isLogStop = false;
-
-      MessageBox.Show("ループ処理を停止しました。");
-    }
-
-    private async void StartLoopInfoLog()
-    {
-      while (_isLogStop)
-      {
-        _logger.Info("Infoを一回出力します");
-        _logger.Warn("Warnを一回出力します");
-        _logger.Error("Errorを一回出力します");
-        _logger.Fatal("Fatalを一回出力します");
-
-        await Task.Delay(0);
-      }
-    }
-
-    private async void StartLoopErrorLog()
-    {
-      while (_isLogStop)
-      {
-        _logger.Info("Infoを一回出力します");
-        _logger.Warn("Warnを一回出力します");
-        _logger.Error("Errorを一回出力します");
-        _logger.Fatal("Fatalを一回出力します");
-
-        await Task.Delay(0);
-      }
-    }
-  }
 }
