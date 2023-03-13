3ba905278f27893cb70431e99b3bd2e1f6b94e1e
shimanamisan <mikan.sup.all@gmail.com>
2023-03-13 19:05:48 +0900

    update: コメントを追加

--- NLogTutorial/MainWindow.xaml.cs
+++ NLogTutorial/MainWindow.xaml.cs
@@ -8,23 +8,58 @@ namespace NLogTutorial
 
     public partial class MainWindow : Window
     {
-        static Logger _logger = LogManager.GetCurrentClassLogger();
+        /// <summary>
+        /// Loggerは各クラスでstatic変数で使用すること
+        /// 新しいLoggerを作成するとオーバーヘッドが発生するので推奨されていない
+        /// </summary>
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
 
+        /// <summary>
+        /// ループ処理を停止するフラグ
+        /// </summary>
         private bool _isLogStop = false;
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// ログ出力のループ処理を開始する
+        /// </summary>
+        /// <param name="sender">コントロールオブジェクト</param>
+        /// <param name="e">イベント引数</param>
         private async void LogOutput(object sender, RoutedEventArgs e)
         {
-            await Task.Run(() =>
+            await Task.Run(async () =>
             {
-                StartLoopInfoLog();
+                _isLogStop = true;
+
+                while (_isLogStop)
+                {
+                    // 以下はinfoフォルダに出力される
+                    _logger.Trace("Traceを一回出力します");
+                    _logger.Debug("Debugを一回出力します");
+                    _logger.Info("Infoを一回出力します");
+
+                    // 以下はerrorフォルダに出力される
+                    _logger.Warn("Warnを一回出力します");
+                    _logger.Error("Errorを一回出力します");
+                    _logger.Fatal("Fatalを一回出力します");
+
+                    await Task.Delay(100);
+                }
             });
         }
 
+        /// <summary>
+        /// 意図的に例外を発生させる
+        /// </summary>
+        /// <param name="sender">コントロールオブジェクト</param>
+        /// <param name="e">イベント引数</param>
         private void LogExceptionOutput(object sender, RoutedEventArgs e)
         {
             try
@@ -44,6 +79,11 @@ namespace NLogTutorial
             }
         }
 
+        /// <summary>
+        /// ループ処理を停止
+        /// </summary>
+        /// <param name="sender">コントロールオブジェクト</param>
+        /// <param name="e">イベント引数</param>
         private void LogRotationStop(object sender, RoutedEventArgs e)
         {
             _isLogStop = false;
@@ -51,24 +91,5 @@ namespace NLogTutorial
             MessageBox.Show("ループ処理を停止しました。");
         }
 
-        private async void StartLoopInfoLog()
-        {
-            _isLogStop = true;
-
-            while (_isLogStop)
-            {
-                // 以下はinfoフォルダに出力される
-                _logger.Trace("Traceを一回出力します");
-                _logger.Debug("Debugを一回出力します");
-                _logger.Info("Infoを一回出力します");
-
-                // 以下はerrorフォルダに出力される
-                _logger.Warn("Warnを一回出力します");
-                _logger.Error("Errorを一回出力します");
-                _logger.Fatal("Fatalを一回出力します");
-
-                await Task.Delay(0);
-            }
-        }
     }
 }
